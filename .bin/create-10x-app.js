3a57a2ea3f4a25b152efe641a28b26ccc59855d1
Luke 10x <spam@luke10x.dev>
2020-10-02 01:01:52 +0300

    ✨ parsing args and fetching

---
+++ .bin/create-10x-app.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const { spawn } = require("child_process");
+const fs = require("fs");
+
+const yargs = require('yargs');
+
+const argv = yargs
+    .option('template', {
+        alias: 't',
+        description: 'Template to use for the new app',
+        type: 'string',
+        required: true,
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+let folderName = '.';
+
+if (argv._.length >= 1) {
+  folderName = process.argv[0];
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+  }
+}
+
+const templateUrls = {
+  battle: 'https://github.com/luke10x/battle/tree/main'
+}
+if (templateUrls[argv.template] === undefined) {
+    console.error("template must be one of these:", Object.keys(templateUrls).join(", "))
+    process.exit(-1);
+}
+const repoUrl = templateUrls[argv.template];
+
+const fetch = spawn("fetcher", [
+  `--url=${repoUrl}`,
+  `--out=${folderName}`
+]);
+
+fetch.on("close", code => {
+  if (code !== 0) {
+    console.error("cloning the template failed!")
+    process.exit(code);
+  } else {
+    console.log("10x app installed! 👍");
+  }
+});
